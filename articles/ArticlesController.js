bbfe98f0674d89fe6291b016312ff3629e2def43
Marcone-Santos1 <marcone.magalhaes203@gmail.com>
2023-01-02 21:12:21 -0300

    add edit articles

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -36,6 +36,52 @@ router.post('/articles/save', (req, res) => {
 
 })
 
+
+router.get('/admin/articles/edit/:id', (req, res) => {
+
+    let id = req.params.id;
+    
+    let validation = id != undefined && id != '' && id != null && !isNaN(id);
+    Article.findByPk(id).then((article) => {
+        if (validation) {
+            Category.findAll().then(categories => {
+                res.render('admin/articles/edit', { article, categories })
+            })
+        } else {
+            res.redirect('/admin/articles')
+        }
+    }).catch(erro => {
+        res.redirect('/admin/articles')
+    })
+});
+
+router.post('/articles/update', (req, res) => {
+    let id = req.body.id;
+    let title = req.body.title
+    let body = req.body.body
+    let categoryId = req.body.category
+
+    let validation = title != undefined && title != '' && title != null && id != undefined && id != '' && id != null;
+
+    if (validation) {
+        Article.update({
+            title,
+            slug: slugify(title),
+            body,
+            categoryId
+        }, {
+            where: {
+                id
+            }
+        }).then(() => {
+            res.redirect('/admin/articles');
+        })
+    } else {
+        res.redirect('/admin/articles/new');
+    }
+
+});
+
 router.post('/articles/delete', (req, res) => {
     let id = req.body.id;
     let validation = id != undefined && id != '' && id != null;
